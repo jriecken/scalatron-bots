6dd866bdb35befabe2b7c334540047d3571b4f23
Jim Riecken <jriecken@gmail.com>
2014-03-20 23:30:28 -0700

    Simple BrownianBot that does nothing but randonly move around avoiding walls just to test that my bot framework works

--- src/main/scala/ca/jimr/scalatron/bot/BrownianBot.scala
+++ src/main/scala/ca/jimr/scalatron/bot/BrownianBot.scala
@@ -1,12 +1,24 @@
 package ca.jimr.scalatron.bot
 
 import ca.jimr.scalatron.data._
+import scala.util.Random
 
 class BrownianBot extends Bot {
   def respond = {
     case WelcomeCommand(name, apocalypse, round, maxslaves) =>
       Seq(StatusResponse("Warming Up..."))
     case ReactCommand(generation, name, time, view, energy, slaves, master, collision, state) =>
-      Seq(MoveResponse(direction = Direction.random)) ++ (if (time % 100 == 0) Seq(SayResponse("Hmm...")) else Seq())
+      if (energy > 0) {
+        Random.shuffle(Direction.All).
+          map { dir => (dir, view.entityAtRelative(dir.toPosition)) }.
+          find { case (_, entity) => !entity.isInstanceOf[Evil] }.
+          map { case (dir, _) =>
+          Seq(MoveResponse(direction = dir), StatusResponse("Moving...")) ++ (if (time % 100 == 0) Seq(SayResponse("Hmm...")) else Seq())
+        }.getOrElse {
+          Seq(StatusResponse("Trapped!"))
+        }
+      } else {
+        Seq(StatusResponse("Oh Cruel World..."))
+      }
   }
 }
